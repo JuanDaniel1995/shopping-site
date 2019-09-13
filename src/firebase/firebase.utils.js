0bdf18a125adcefdc1349ef8018d87fbad7e3c3d
Juan Daniel Vargas <juandanielvargascordero@gmail.com>
2019-09-13 11:08:11 -0600

    Final project for a course in the platform Udemy, initial commit

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,45 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "shopping-site-cefe0.firebaseapp.com",
+  databaseURL: "https://shopping-site-cefe0.firebaseio.com",
+  projectId: "shopping-site-cefe0",
+  storageBucket: "",
+  messagingSenderId: "35589021946",
+  appId: "1:35589021946:web:e1e30a7b94da3e660fa943"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef  = firestore.doc(`users/${userAuth.uid}`);
+  const userSnapshot = await userRef.get();
+  if(!userSnapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
